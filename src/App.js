751e695d6a9b15f46a4a4c0d16ab841d2f7c83ae
urmila1706 <raut.urmila88@gmail.com>
2024-04-27 22:14:53 +0530

    uptimizing with useMemo in Listitem project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {useCallback, useState,useMemo} from 'react';
 import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './UI/Button/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [listTitle,setListTitle]=useState('My List');
+  const changeTitleHandler=useCallback(()=>{
+    setListTitle('New Title');
+  },[]);
+  const listItems=useMemo(()=>[5,3,1,10,9],[]);
+  return ( <div className="app">
+<DemoList title={listTitle} items={listItems}/>
+<Button onClick={changeTitleHandler}>Change List Title</Button>
+  </div>
+
+);
+};
 
 export default App;
